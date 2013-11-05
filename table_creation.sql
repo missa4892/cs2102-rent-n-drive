2fccb18dc798377ff097ebdb54213ee56c16b658
missa4892 <missa4892@gmail.com>
2013-11-05 12:17:39 +0800

    latest tables code

--- table_creation.sql
+++ table_creation.sql
@@ -1,8 +1,8 @@
 CREATE TABLE VEHICLES (
   vehicleNo VARCHAR (20) PRIMARY KEY,
-  modelType VARCHAR (20),
-  color VARCHAR (20), 
-  price NUMERIC (10),
+  modelType VARCHAR (20) NOT NULL,
+  color VARCHAR (20)NOT NULL, 
+  price NUMERIC (10) NOT NULL,
   branch VARCHAR (20) CHECK (branch = 'hougang' or branch = 'tampines' or branch = 'woodlands'),
   available NUMBER(1) CHECK (available ='1' or available = '0')
 );
@@ -34,16 +34,7 @@ CREATE TABLE CARS (
   sizeType VARCHAR (20) CHECK (sizeType = 'small' or sizeType = 'standard' or sizeType = 'suv' or sizeType = 'special'),
   mileage NUMERIC (10),
   cc VARCHAR (20),
-  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES
-);
-
-CREATE TABLE MINIBUS (
-  fuelType VARCHAR (20) CHECK (fuelType = 'petrol'or fuelType = 'gas' or fuelType = 'diesel'),
-  gear VARCHAR (20) CHECK (gear = 'auto' or gear = 'manual'),
-  sizeType VARCHAR (20) CHECK (sizeType = 'small' or sizeType = 'standard' or sizeType = 'suv' or sizeType = 'special'),
-  mileage NUMERIC (10),
-  cc VARCHAR (20),
-  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES
+  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES ON DELETE CASCADE
 );
 
 CREATE TABLE MOTORCYCLES (
@@ -52,25 +43,24 @@ CREATE TABLE MOTORCYCLES (
   sizeType VARCHAR (20) CHECK (sizeType = 'small' or sizeType = 'standard' or sizeType = 'suv' or sizeType = 'special'),
   mileage NUMERIC (10),
   cc VARCHAR (20),
-  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES
+  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES ON DELETE CASCADE
 );
 
 CREATE TABLE BUS (
+  busType VARCHAR (20) CHECK (busType = 'bus' or busType = 'minivan'),
   fuelType VARCHAR (20) CHECK (fuelType = 'petrol'or fuelType = 'gas' or fuelType = 'diesel'),
   gear VARCHAR (20) CHECK (gear = 'auto' or gear = 'manual'),
-  sizeType VARCHAR (20) CHECK (sizeType = 'small' or sizeType = 'standard' or sizeType = 'suv' or sizeType = 'special'),
-  mileage NUMERIC (10),
-  cc VARCHAR (20), 
-  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES
+  sizeType NUMERIC (2) CHECK (sizeType = '10' or sizeType = '20' or sizeType = '30' or sizeType = '40'),
+  vehicleNo VARCHAR (20) PRIMARY KEY REFERENCES VEHICLES ON DELETE CASCADE
 );
 
 CREATE TABLE ACCESSORIES (
   vehicleNo VARCHAR (20),
-  accessID VARCHAR (20),
+  accessID NUMERIC (20),
   uname VARCHAR (20),
   qty NUMERIC (1),
-  forType NUMERIC (1)CHECK (accessID = '1' or accessID = '2' or accessID = '3' or accessID = '4'),
-    PRIMARY KEY(vehicleNo, accessID),
+  forType NUMERIC (1)CHECK (forType = '1' or forType = '2' or forType = '3' or forType = '4'),
+  PRIMARY KEY(vehicleNo, accessID),
   FOREIGN KEY (vehicleNo) REFERENCES VEHICLES (vehicleNo)
 );
 
