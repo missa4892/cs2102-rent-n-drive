65a9854a7ad1abc7a1e530addaccf69acb4f9e09
missa4892 <missa4892@gmail.com>
2013-11-05 15:34:19 +0800

    car insert statements and updated table creation

---
+++ insert_car.sql
@@ -0,0 +1,43 @@
+INSERT INTO VEHICLES VALUES ('21', 'Land Rover Freelander', 'Silver', 'hougang', '1', '288', '300');
+INSERT INTO VEHICLES VALUES ('22', 'Hyundai i45', 'Black', 'tampines', '1', '145', '145');
+INSERT INTO VEHICLES VALUES ('23', 'Kia Cerato', 'White', 'woodlands', '1', '70', '80');
+INSERT INTO VEHICLES VALUES ('24', 'Nissan Cefiro', 'Violet', 'hougang', '1', '90', '90');
+INSERT INTO VEHICLES VALUES ('25', 'Mitsubishi Grandis', 'Blue', 'tampines', '1', '139', '150');
+INSERT INTO VEHICLES VALUES ('26', 'Subaru Forrester XT', 'Blue', 'woodlands', '1', '130', '145');
+INSERT INTO VEHICLES VALUES ('27', 'Toyota Picnic', 'Silver', 'hougang', '1', '140', null);
+INSERT INTO VEHICLES VALUES ('28', 'Audi TTS Roadster', 'White', 'tampines', '0', '522', '629');
+INSERT INTO VEHICLES VALUES ('29', 'Toyota Vios', 'Silver', 'woodlands', '1', '78', '78');
+INSERT INTO VEHICLES VALUES ('30','BMW 523i', 'Black', 'hougang', '0', '262', '262');
+INSERT INTO VEHICLES VALUES ('31', 'Nissan Cefiro', 'Silver', 'tampines', '1', '552', '552');
+INSERT INTO VEHICLES VALUES ('32', 'BMW 523i', 'Black', 'woodlands', '1', '222', '222');
+INSERT INTO VEHICLES VALUES ('33', 'Toyota Vios', 'Black', 'hougang', '0', '78', '78');
+INSERT INTO VEHICLES VALUES ('34', 'Audi TTS Roadster', 'Blue', 'tampines', '0', '522', '629');
+INSERT INTO VEHICLES VALUES ('35','BMW 523i', 'Black', 'woodlands', '0', '262', '262');
+INSERT INTO VEHICLES VALUES ('36', 'Mitsubishi Grandis', 'Blue', 'hougang', '1', '139', '150');
+INSERT INTO VEHICLES VALUES ('37', 'Subaru Forrester XT', 'Black', 'tampines', '1', '130', '145');
+INSERT INTO VEHICLES VALUES ('38', 'Hyundai i45', 'White', 'woodlands', '1', '145', '145');
+INSERT INTO VEHICLES VALUES ('39', 'Land Rover Freelander', 'Black', 'hougang', '1', '288', '300');
+INSERT INTO VEHICLES VALUES ('40', 'Toyota Picnic', 'Silver', 'hougang', '1', '140', null);
+
+
+INSERT INTO CARS VALUES ('petrol ', 'auto', 'standard', '12457', '3192', 'SUV', '21');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '45287', '1999', 'Luxury Sudan', '22');
+INSERT INTO CARS VALUES ('petrol','auto', 'standard', '454', '1599', 'Sedan', '23');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '45124', '2458', 'Luxury Sudan', '24');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'special', '54824', '1236' 'MPV', '25');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '5248', '2499' 'SUV', '26');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'special', '14526', '1998', 'MPV', '27');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'small', '14526', '1999', 'Sports', '28');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '12665', '1498', 'Sedan', '29');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '1114', '2171', 'Luxury Sedan', '30');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '45124', '2458', 'Sudan','31');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '45124', '2458', 'MPV','32');
+INSERT INTO CARS VALUES ('diesel', 'auto', 'special', '14526', '1998', 'Sedan','33');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'special', '14526', '1998', 'MPV','34');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'special', '14526', '1998', 'MPV','35');
+INSERT INTO CARS VALUES ('diesel', 'auto', 'standard', '45124', '2458', 'Sudan','36');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'standard', '45124', '2458', 'Luxury Sudan','37');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'small', '14526', '1999', 'Sports','38');
+INSERT INTO CARS VALUES ('diesel', 'auto', 'standard', '45124', '2458', 'SUV','39');
+INSERT INTO CARS VALUES ('petrol', 'auto', 'special', '14526', '1998', 'MPV','40');
+
